30fdc1fb6b35f8e6432f6f9bdf98b88cd1562ede
Brenden Bilson <brendenbilson5@gmail.com>
2025-02-25 12:21:33 -0800

    Update graph_analysis.py

--- Social and Large-Scale Networks (Assignment 2)/graph_analysis.py
+++ Social and Large-Scale Networks (Assignment 2)/graph_analysis.py
@@ -27,7 +27,21 @@ def partition(graph, n):
         edge = max(nx.edge_betweenness_centrality(graph).items(), key=lambda x: x[1])[0]
         graph.remove_edge(*edge)
 
+def onClick(event, pos, node_sizes):
+    x_click = event.xdata
+    y_click = event.ydata
+    for node, position in pos.items():
+        x_center, y_center = position
+        distance = math.sqrt((x_center - x_click) ** 2 + (y_center - y_click) ** 2)
+        radius = node_sizes[int(node)] / 2
+        if distance < radius:
+            print(f"{position}, {distance}, {radius}")
+            break
+
 def plotGraph(graph, args):
+    pos = nx.spring_layout(nx.read_gml(sys.argv[1]))
+    node_sizes = []
+    node_colors = []
     if 'C' in args.upper():
         dict_clustering_coefficients = nx.clustering(graph)
         clustering_coefficients = list(dict_clustering_coefficients.values())
@@ -35,27 +49,36 @@ def plotGraph(graph, args):
         cluster_max = max(clustering_coefficients)
         min_pixel = 100
         max_pixel = 500
-        node_sizes = []
         for c in clustering_coefficients:
             p = (c - cluster_min) / (cluster_max - cluster_min)
             size = min_pixel + (max_pixel - min_pixel) * p
             node_sizes.append(size)
-        node_colors = []
         degree_values = list(dict(graph.degree()).values())
         max_degree = max(degree_values)
         for d in degree_values:
             s = (d / max_degree)
             node_colors.append((s, 0, 1))
-            
+    else:
+        node_sizes = [100] * len(graph)
+        node_colors = 'blue'
     if 'N' in args.upper():
-        #neighborhood overlap
-        exit
+        edge_labels = {}
+        for u, v in graph.edges():
+            common_neighbors = len(list(nx.common_neighbors(graph, u, v)))
+            join_neighbors = len(set(graph.neighbors(u)).union(set(graph.neighbors(v)))) - 2
+            if join_neighbors <= 0:
+                overlap = 0
+            else:
+                overlap = common_neighbors / join_neighbors
+            edge_labels[(u, v)] = round(overlap, 2)
+        nx.draw_networkx_edge_labels(graph, edge_labels=edge_labels, pos=pos)
     if 'P' in args.upper():
         #color nodes according to specified attributes
         exit
 
-    nx.draw(graph, node_size=node_sizes, node_color=node_colors, with_labels=True, pos=nx.spring_layout(graph, k=0.15, iterations=20))
+    nx.draw(graph, node_size=node_sizes, node_color=node_colors, pos=pos, with_labels=True)
     plt.suptitle("Graph")
+    plt.connect('button_press_event', lambda event: onClick(event, pos, node_sizes))
     plt.show()
 
 def main():
